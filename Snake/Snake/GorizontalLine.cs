40be5e91b93d6401ba9d076b9b77a052325467c5
Andrey0812 <andrey_08.12@mail.ru>
2018-04-26 21:08:03 +0300

    Создали класс. Горизонтальная линия

---
+++ Snake/Snake/GorizontalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class GorizontalLine
+    {
+        List<Point> pList = new List<Point>();
+
+        public GorizontalLine(int x1, int x2, int y, char sym)
+        {
+            for (int i = x1; i<=x2; i++)
+            {
+                pList.Add(new Point(i, y, sym));
+            }
+
+
+        }
+
+        public void Draw()
+        {
+            foreach(Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
