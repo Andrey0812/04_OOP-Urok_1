37a403610ed2a75eb157914ee8b1a8d4a9f3bb6f
Andrey0812 <andrey_08.12@mail.ru>
2018-05-20 12:22:03 +0300

    Создание перечисления с направлениями. Комментирование написанной программы

---
+++ Snake/Snake/Direction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    //Создаём перечисления, которые буду использоваться в программе как ключевые слова
+    enum Direction 
+    {
+        //список с возможными значениями направлений движения змейки
+        LEFT,   //влево
+        RIGHT,  //вправо
+        UP,     //вверх
+        DOWN    //вниз
+    }
+}
---
+++ Snake/Snake/Snake.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Snake : oLine
+    {
+        public Snake(Point tail,        int lenght,     Direction direction)
+        //          точка хваоста,      длина змейки,   направление
+        {
+            pList = new List<Point>();      //список точек
+
+            for(int i =0; i < lenght; i++)
+            {
+                Point p = new Point(tail);  //создание точки подобной точке хвоста
+                p.Move(i, direction);       //определение координаты следующей точки змейки на основании направления 'direction'
+                pList.Add(p);               //добавление точки в список
+            }
+        }
+    }
+}
