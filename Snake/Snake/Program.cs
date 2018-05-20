1959f7c4cbfb74e0edbbdc6983ab1f0a551f0338
Andrey0812 <andrey_08.12@mail.ru>
2018-05-20 12:17:26 +0300

    Revert "Revert "Создание класса "Змейка".""
    This reverts commit 128aea69af4c5b95e806494d169b44421705504a.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,10 +16,10 @@ namespace Snake
             Console.SetWindowSize(160, 50);
             
             //создаём объекты - линия горизонтальная и линия вертикальная
-            GorizontalLine g1Line = new GorizontalLine(0, 158, 0,'+');  //Верхняя горизонтальная линия
-            VerticalLine v1Line = new VerticalLine(0, 0, 49, '+');      //Левая вертикальная линия
-            GorizontalLine g2Line = new GorizontalLine(0, 158, 49, '+');//Нижняя горизонтальная линия
-            VerticalLine v2Line = new VerticalLine(158, 0, 49, '+');    //Правая вертикальная линия
+            GorizontalLine g1Line = new GorizontalLine(0, 158, 0,'-');  //Верхняя горизонтальная линия
+            VerticalLine v1Line = new VerticalLine(0, 0, 49, '|');      //Левая вертикальная линия
+            GorizontalLine g2Line = new GorizontalLine(0, 158, 49, '-');//Нижняя горизонтальная линия
+            VerticalLine v2Line = new VerticalLine(158, 0, 49, '|');    //Правая вертикальная линия
 
             //рисуем созданные линии
             g1Line.Draw();
@@ -28,8 +28,9 @@ namespace Snake
             v2Line.Draw();
 
             //рисуем точку
-            Point l = new Point(55,36,'*');
-            l.Draw();
+            Point l = new Point(4,5, '*');
+            Snake snake = new Snake(l, 4, Direction.LEFT); //создание змейки с начальной точной l длиной 4единицы вправо от начальной точки
+            snake.Draw();
 
             //ждём нажатия Enter, что бы консоль не закрывалась сразу после исполнения программы
             Console.ReadLine();
